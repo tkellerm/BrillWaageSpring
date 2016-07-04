248759dfddad09745bdac53940c0022391180aa2
marco <marco@morca.de>
2016-07-04 13:46:38 +0200

    edpconnectionfactory

---
+++ src/main/java/de/abasgmbh/brill/edp/EdpConnectionFactory.java
@@ -0,0 +1,19 @@
+/**
+ * creation date: Jul 04, 2016
+ * first author: marco
+ * maintained by: marco
+ * <p>
+ * (C) Copyright abas Software AG, Karlsruhe, Germany; 2016
+ */
+package de.abasgmbh.brill.edp;
+
+import de.abas.ceks.jedp.EDPSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EdpConnectionFactory {
+
+    public EDPSession getSession() {
+        return null;
+    }
+}
