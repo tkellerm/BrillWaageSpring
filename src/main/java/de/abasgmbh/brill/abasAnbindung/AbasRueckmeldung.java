e0593c451a155a8dff472a2a5ec9591c4222c7e2
tkellermann <tobias.kellermann@abas.de>
2016-07-04 14:59:04 +0200

    Anpassung WaagenReader

--- src/main/java/de/abasgmbh/brill/abasAnbindung/AbasRueckmeldung.java
+++ src/main/java/de/abasgmbh/brill/abasAnbindung/AbasRueckmeldung.java
@@ -1,109 +1,48 @@
 package de.abasgmbh.brill.abasAnbindung;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import sun.security.acl.OwnerImpl;
-import de.abas.ceks.jedp.CantBeginSessionException;
 import de.abas.ceks.jedp.CantChangeFieldValException;
-import de.abas.ceks.jedp.CantReadSettingException;
 import de.abas.ceks.jedp.CantSaveException;
-import de.abas.ceks.jedp.ConnectionLostException;
 import de.abas.ceks.jedp.EDPEditAction;
 import de.abas.ceks.jedp.EDPEditRefType;
 import de.abas.ceks.jedp.EDPEditor;
 import de.abas.ceks.jedp.EDPException;
-import de.abas.ceks.jedp.EDPFactory;
 import de.abas.ceks.jedp.EDPQuery;
 import de.abas.ceks.jedp.EDPSession;
-import de.abas.ceks.jedp.EDPVariableLanguage;
 import de.abas.ceks.jedp.InvalidQueryException;
 import de.abas.ceks.jedp.InvalidRowOperationException;
 import de.abas.ceks.jedp.StandardEDPSelection;
 import de.abas.ceks.jedp.StandardEDPSelectionCriteria;
 import de.abasgmbh.brill.config.Waage;
+import de.abasgmbh.brill.edp.EdpConnectionFactory;
 
-public class AbasRueckmeldung implements Runnable {
+@Component 
+public class AbasRueckmeldung  {
 
-	private static final int TIMEOUT =  86400000; //Timeout ein Tag
+//	Holt aus der EdpFactory die EdpSession
+	
+	@Autowired 
+	EdpConnectionFactory edpConnectionFactory;
+	
 	private static Logger log = Logger.getLogger(AbasRueckmeldung.class.getName());
-	final String arbeitschein = "" ;
-	final Double menge=0.0;
-	
-	private String server;
-	private Integer port;
-	private String mandant;
-	private String passwort;
-	
-	private String Fehler;
-	private EDPSession edpSession;
-	
-	private Double rotGrenze;
-	private Double gelbGrenze;
-	
-	private Boolean isConnected;
 	
 	
 	
-	@Override
-	public void run() {
-		
-//		edp prozess session aufbauen
-		this.edpSession = EDPFactory.createEDPSession (); 
-		Boolean errorFlag = false;
-		while (!errorFlag ) {
-//			Todo : Überwachung neu machen
-//			if (!waageConfiguration.pidFileexists()) {
-//				log.info("pid-File existiert nicht mehr");
-//				errorFlag = true;
-//			}
-			Boolean connetedt = edpSession.isConnected();
-			if (connetedt) {
-				try {
-					
-					String version = edpSession.getABASVersion();
-//					log.trace("Schleife AbasRueckmeldung " + version);
-					Thread.sleep(1000);
-				} catch (CantReadSettingException e1) {
-					// TODO Auto-generated catch block
-					log.error(e1);
-					edpSession.endSession();
-					this.isConnected = false;
-				}catch (ConnectionLostException e) {
-					log.error(e);
-					edpSession.endSession();
-					this.isConnected = false;
-				} catch (InterruptedException e) {
-					log.error(e);
-				}
-			}
-			if (!edpSession.isConnected()) {
-				try {
-					sessionAufbauen(server, port, mandant, passwort);
-				} catch (CantBeginSessionException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					log.error(e);
-					errorFlag = true;
-				}
-			}
-		}
-		
-	}
-	
-	public synchronized Rueckmeldung meldung(Rueckmeldung rueckMeldung) throws EDPException {
+	public  Rueckmeldung meldung(Rueckmeldung rueckMeldung , Waage waage) throws EDPException {
 		Integer wert = 0;
-		if (!this.edpSession.isConnected()) {
-			sessionAufbauen(this.server, this.port, this.mandant, this.passwort);
-		}
-				
-		EDPEditor editor = this.edpSession.createEditor();
+		
+		EDPSession edpSession = this.edpConnectionFactory.getSession();
+		EDPEditor editor = edpSession.createEditor();
 		erzeugeRueckmeldung(rueckMeldung , editor);
-		EDPQuery query = this.edpSession.createQuery();
+		EDPQuery query = edpSession.createQuery();
 		Double prozent = selectOffeneMengeBa(rueckMeldung, query); 
-		if (prozent > this.gelbGrenze) {
-//			Gr�nLeuchtet
+		if (prozent > waage.getGelbGrenze()) {
+//			GrünLeuchtet
 			wert = 1;
-		}else if (prozent <= this.gelbGrenze && prozent > this.rotGrenze ) {
+		}else if (prozent <= waage.getGelbGrenze() && prozent > waage.getRotGrenze() ) {
 //			Gelb leuchtet
 			wert = 2; 
 		}else {
@@ -111,7 +50,7 @@ public class AbasRueckmeldung implements Runnable {
 			wert = 3;
 		}
 		rueckMeldung.setLed(wert);
-		log.trace("R�ckgabe Meldung : Prozent :" + prozent + " rotGrenze : " + this.rotGrenze.toString() + "gelbGrenze : " + this.gelbGrenze.toString() +"  R�ckgabe Wert :" + wert);
+		log.trace("Rückgabe Meldung : Prozent :" + prozent + " rotGrenze : " + waage.getRotGrenze().toString() + "gelbGrenze : " + waage.getGelbGrenze().toString() +"  R�ckgabe Wert :" + wert);
 		return rueckMeldung ;
 	}
 	
@@ -166,7 +105,7 @@ public class AbasRueckmeldung implements Runnable {
 	        log.trace("Begin EDP-EDITOR f�r R�ckmeldung " + asnum );
 		editor.beginEdit(EDPEditAction.DONE, databaseName , groupName,EDPEditRefType.NUMSW , asnum);
 		
-//		Bemerkung f�llen 
+//		Bemerkung füllen 
 		try {
 			editor.setFieldVal("comments" , "Nr: " + rueckMeldung.getLaufendeNummer() + " W: " + rueckMeldung.getWaagenNummer() + " NG: " + rueckMeldung.getNettoGewicht().toString() + " refG: " + rueckMeldung.getRefgewicht().toString() );
 			editor.setFieldVal("printImmediately", "1");
@@ -192,80 +131,5 @@ public class AbasRueckmeldung implements Runnable {
 		log.trace("Ende EDP-EDITOR f�r R�ckmeldung " + asnum );
 	}
 
-//	public AbasRueckmeldung(WaageConfiguration configuration) {
-//		if (configuration != null) {
-//			this.server = configuration.getEdpServer();
-//			this.mandant = configuration.getMandant();
-//			this.port =  configuration.getPort();
-//			this.passwort = configuration.getPassword();
-//			this.gelbGrenze = configuration.getGelbGrenze();
-//			this.rotGrenze = configuration.getRotGrenze();
-////			this.waageConfiguration = configuration;
-//		}else {
-//			String error = "Die Konfiguration hat leider den Wert null!";
-//			log.error(error);
-//			throw new NullPointerException(error);
-//		}
-//		
-		public AbasRueckmeldung(Waage waage) {
-			if (waage != null) {
-				
-				this.gelbGrenze = waage.getGelbGrenze();
-				this.rotGrenze = waage.getRotGrenze();
-				
-			}else {
-				String error = "Die Waage hat leider den Wert null!";
-				log.error(error);
-				throw new NullPointerException(error);
-			}
-	}
-
-	
-	
-
-	private void sessionAufbauen(String server, int port, String mandant, String passwort) throws CantBeginSessionException   
-    { 
-			log.trace("Session wird versucht aufzubauen Server: " + server + " Port : " + port + " Mandant : " + mandant + " Passwort : " + passwort);
-    	  		try {
-    	  			this.edpSession.setConnectTimeout(TIMEOUT);
-    	  			this.edpSession.beginSession(server , port, mandant, passwort, "Waage");
-    	  			this.isConnected = true;
-    	  		} catch (CantBeginSessionException ex) 
-    	  			{
-    	  			this.isConnected = false;
-    	  			log.error(ex);
-    	  			throw new CantBeginSessionException("FEHLER\n EDP Session kann nicht gestartet werden\n" , ex);
-    	  			}
-                 log.trace("Session wurde aufgebaut");
-              this.edpSession.setVariableLanguage(EDPVariableLanguage.ENGLISH);
-        
-	        
-    }
-
-	public Boolean isConnected() {
-	
-		if (this.edpSession != null) {
-			if (this.edpSession.isConnected()) {
-				//			check Version
-				try {
-					String version = edpSession.getABASVersion();
-					if (version != null) {
-						return true;
-					} else
-						return false;
-				} catch (CantReadSettingException e1) {
-					// TODO Auto-generated catch block
-					log.error("Pr�fung isConnected fehlgeschlagen ", e1);
-					return false;
-
-				} catch (ConnectionLostException e) {
-					return false;
-				}
-			} else
-				return false;
-		}else 
-			return false;
 		
-	}
-	
 }
--- src/main/java/de/abasgmbh/brill/controller/WaageReader.java
+++ src/main/java/de/abasgmbh/brill/controller/WaageReader.java
@@ -7,32 +7,213 @@
  */
 package de.abasgmbh.brill.controller;
 
+import de.abas.ceks.jedp.EDPException;
+import de.abasgmbh.brill.abasAnbindung.AbasRueckmeldung;
+import de.abasgmbh.brill.abasAnbindung.Rueckmeldung;
 import de.abasgmbh.brill.config.Waage;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WaageReader {
 
-    Logger log = Logger.getLogger(WaagenController.class);
+    private Logger log = Logger.getLogger(WaagenController.class);
+    
+    private final String ANFANGS_ZEICHEN =  Character.toString((char)2);
+	private final String ENDE_ZEICHEN =  Character.toString((char)3);
 
     private final Waage waage;
     private final WaageConnection wconn;
-
+    
+    @Autowired
+    AbasRueckmeldung abasrueckmeldung;
+    
     public WaageReader(Waage waage, WaageConnection wconn) {
         this.waage = waage;
         this.wconn = wconn;
     }
 
-    public void start() {
-
+    public void start() throws EDPException {
+    	
+    	String waageName = this.waage.getName();
+    	ArrayList<String> rueckschlange = new ArrayList<String>();
+    	
         try {
-            String line = wconn.readline();
+        	Boolean rueckMeldungActive = false;
+            Boolean errorFlag = false;
+        	
+        	
+        	while (!errorFlag) {
+				String line = wconn.readline();
+				log.info(waageName + " von Waage : " + line);
+				
+				if (line != null) {
 
+					if (rueckMeldungActive) {
+						if (line.contains(ENDE_ZEICHEN)) {
+							String teilString[] = line.split(ENDE_ZEICHEN);
+							if (teilString.length > 0) {
+								line = line + teilString[0];
+								rueckschlange.add(line);
+								log.trace(waageName + " RückmeldungString : "
+										+ line);
+								rueckMeldungActive = false;
+							}
+							for (int i = 1; i < teilString.length; i++) {
+								if ((teilString[i].contains(ENDE_ZEICHEN))
+										& (teilString[i]
+												.contains(ANFANGS_ZEICHEN))) {
+									rueckschlange.add(teilString[i]);
+									log.trace(waageName
+											+ " RückmeldungString : "
+											+ teilString[1]);
+									rueckMeldungActive = false;
+								} else if (teilString[i]
+										.contains(ANFANGS_ZEICHEN)) {
+									line = teilString[i];
+									rueckMeldungActive = true;
+								}
+							}
+						} else if (line.contains(ANFANGS_ZEICHEN)) {
+							String teilString[] = line.split(ANFANGS_ZEICHEN);
+							if (teilString.length > 0) {
+								line = teilString[0];
+								rueckMeldungActive = true;
+							}
 
+						}
+					} else {
+						//					Falls kein Anfangszeichen enthalten ist, wird auch keine Rückmeldung erzeugt.
+
+						if (line != null) {
+							if (line.contains(ANFANGS_ZEICHEN)) {
+								String teilString[] = line
+										.split(ANFANGS_ZEICHEN);
+								if (teilString.length > 1) {
+									//							Der Start beginnt ja erst nach dem Startzeichen
+									for (int i = 1; i < teilString.length; i++) {
+										if (teilString[i]
+												.contains(ENDE_ZEICHEN)) {
+											rueckschlange.add(teilString[1]);
+											log.trace(waageName
+													+ " RückmeldungString : "
+													+ teilString[1]);
+											rueckMeldungActive = false;
+										} else {
+											line = teilString[i];
+											rueckMeldungActive = true;
+										}
+									}
+								}
+							}
+						} else {
+							log.error("Die Variable inputstring ist null.Es erfolgt einreconnect");
+							//						Todo: reconnect = true;
+						}
+
+					}
+
+				}
+				
+				verarbeiteteRueckmeldungsSchlange(rueckschlange);
+			}
+        	
         } catch (IOException e) {
             log.error(e);
+            
         }
 
     }
+
+	private void verarbeiteteRueckmeldungsSchlange(
+			ArrayList<String> rueckschlange) throws EDPException, IOException {
+
+		for (String rueckMeldungString : rueckschlange) {
+
+			Rueckmeldung rueckMeldung = new Rueckmeldung(rueckMeldungString);
+			
+	        if (rueckMeldung.isRueckmeldung()) {
+	        	rueckMeldung = this.abasrueckmeldung.meldung(rueckMeldung , this.waage);
+	        	Integer led = rueckMeldung.getLed(); 
+	            switch (led) {
+				case 1:
+	//				grüne Lampe anschalten
+					rueckmeldungAnWaageSenden(LEDS.GREEN,rueckMeldung.getOfMenge());
+					break;
+				case 2:
+	//				gelbe Lampe anschalten
+					rueckmeldungAnWaageSenden(LEDS.YELLOW,rueckMeldung.getOfMenge());
+					break;
+				case 3:
+	//				rote Lampe anschalten
+					rueckmeldungAnWaageSenden(LEDS.RED,rueckMeldung.getOfMenge());
+					break;
+				case 4:
+	//				leiser Piepser anschalten
+					rueckmeldungAnWaageSenden(LEDS.PIEPSLEISE,rueckMeldung.getOfMenge());
+					break;
+				case 5:
+	//				lauter Piepser anschalten
+					rueckmeldungAnWaageSenden(LEDS.PIEPSLAUT,rueckMeldung.getOfMenge());
+					break;	
+				default:
+					log.info("Break in Case Wert LED : " + led );
+					break;
+				}
+	        	
+			}
+        }
+		
+		
+	}
+	
+	private void rueckmeldungAnWaageSenden(LEDS led, Double ofMenge) throws IOException {
+    	lampeAnschalten(led);
+    	sendText("offene Menge : " + ofMenge.toString());
+    	labelDrucken();
+    	waageZurücksetzen();
+	}
+
+	private void fehlerAnWaage(String string) throws IOException {
+		lampeAnschalten(LEDS.PIEPSLAUT);
+		sendText(string);
+		waageZurücksetzen();
+		
+	}
+    
+	private void sendText(String string) throws IOException {
+		String befehl = "<CA" + string + "><NO" + this.waage.getTextdauer().toString() + "><CC>";
+    	sendMessage(befehl);
+    	log.info(this.waage.getName() + " <CA" + " " + string);
+		
+	}
+	
+	private void waageZurücksetzen() throws IOException {
+		String befehl = "<FR>";
+//		<VS0020> löscht den Betriebsauftrag aus der Waage 
+		befehl = befehl + "<VS0020>";
+    	sendMessage(befehl);
+    	log.info(this.waage.getName() + " Waage zurücksetzen " + befehl);
+	} 
+	private void labelDrucken() throws IOException{
+		String befehl = "<FP099>";
+    	sendMessage(befehl);
+    	log.info(this.waage.getName()  + " Label drucken" + " " + befehl);
+		
+	}
+	
+	private void lampeAnschalten(LEDS led) throws IOException {
+		// TODO Auto-generated method stub
+    	String befehl = led.getAnschaltCmdString() + "<NO" + this.waage.getLeuchtdauer().toString() + ">" + led.getAusschaltCmdString();
+    	sendMessage(befehl);
+    	log.info(this.waage.getName() + " Lampen Befehl : " + led.name() + " " + befehl);
+	
+	}
+	  private void sendMessage(String s) throws IOException {
+		  wconn.writeString(s);
+	  }
+	
 }
